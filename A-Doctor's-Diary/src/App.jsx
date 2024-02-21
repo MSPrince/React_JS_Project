9afdd238f68a649fdf3ec6828cc9f640c506373d
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-02-21 23:50:52 +0530

    Initial commit

---
+++ A-Doctor's-Diary/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './component/navbar/Navbar';
+import Footer from './component/footer/Footer';
+import Homepage from './component/home-page/Homepage';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Homepage/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ A-Doctor's-Diary/src/component/labtest/Labtest.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+function Labtest() {
+  return (
+    <>
+      <Navbar />
+      <div>gkjgkj</div>
+    </>
+  );
+}
+
+export default Labtest
